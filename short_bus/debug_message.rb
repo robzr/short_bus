23f8004a6cf27e9c5e6929265d4537a94f59c9a2
Rob Zwissler <rob@zwissler.org>
2016-06-07 14:12:45 -0700

    gratuitous refactoring

--- short_bus/debug_message.rb
+++ short_bus/debug_message.rb
@@ -1,11 +1,8 @@
 module DebugMessage
   def debug_message(message)
-    @debug_message_fh ||= STDERR
-    @debug_message_fh.printf(
+    (@debug_message_fh ||= STDERR).printf(
       "%s::%s\n",
-      caller.first
-        .sub(/^.*\//, '')
-        .sub(/:.*$/, ''),
+      caller.first.sub(/^.*\/([^:]*).*/, '\1'),
       message
     ) if @debug
   end
